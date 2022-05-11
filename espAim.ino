9acf5f151db02afe19d270ff1d9ce89ac5cc973d
tom-salden <tva.salden@gmail.com>
2022-05-11 11:39:08 +0200

    Cleaned up code a bit and added some structs

--- espAim.ino
+++ espAim.ino
@@ -5,18 +5,36 @@ int servoPin = 13;
 int servoMin = 500;
 int servoMax = 2500;
 int servoCenter = (servoMax + servoMin) / 2;
-bool objectFront = true
+
+struct GPSLocation {
+    float latitude;
+    float longitude;
+    float altitude;
+};
+
+struct trackerServoData {
+    int directionPin;
+    int directionMin;
+    int directionMax;
+    int directionCenter;
+    int directionLocation;
+    int altitudePin;
+    int altitudeMin;
+    int altitudeMax;
+    int altitudeCenter;
+    int altitudeLocation;
+    bool objectFront;
+};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
-  
-  float lat1 = 51.997842;
-  float lon1 = 4.374279;
 
-  float lat2 = 52.05110410396898;
-  float lon2 = 4.468350219726553;
+  GPSLocation currentLocation = {51.997842, 4.374279, 0};
+  GPSLocation planeLocation = {52.05110410396898, 4.468350219726553, 1000};
+
+  trackerServoData servoData = {servoPin, servoMin, servoMax, servoCenter, servoCenter, 0, 0, 0, 0, 0, true};
 
   headingservo.setPeriodHertz(50);
   headingservo.attach(servoPin, servoMin, servoMax);
@@ -24,8 +42,10 @@ void setup() {
   delay(1000);
   headingservo.write(servoMax);
   delay(1000);  
-  //Serial.println(heading(lat1,lon1,lat2,lon2));
-  setServo(headingservo, lat1, lon1, lat2, lon2);
+  servoData = setDirection(servoData, currentLocation, planeLocation);
+  Serial.print("Servo set to number:");
+  Serial.println(servoData.directionLocation);
+  headingservo.write(servoData.directionLocation);
 }
 
 float deg2rad(float deg) {
@@ -35,12 +55,12 @@ float rad2deg(float rad) {
   return (rad * 4068) / 71;
 }
 
-float heading(float lat1,float lon1,float lat2,float lon2) { //Function to determine the heading of the object
+float heading(GPSLocation curLoc, GPSLocation newLoc) { //Function to determine the heading of the object
   //Convert all coordinates to radians
-  lat1 = deg2rad(lat1);
-  lon1 = deg2rad(lon1);
-  lat2 = deg2rad(lat2);
-  lon2 = deg2rad(lon2);
+  float lat1 = deg2rad(curLoc.latitude);
+  float lon1 = deg2rad(curLoc.longitude);
+  float lat2 = deg2rad(newLoc.latitude);
+  float lon2 = deg2rad(newLoc.longitude);
 
   float x = cos(lat2) * sin(lon2-lon1);
   float y = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(lon2-lon1);
@@ -50,33 +70,30 @@ float heading(float lat1,float lon1,float lat2,float lon2) { //Function to deter
   return angle; //Angle relative to North
 }
 
-void setServo(Servo directionservo, float lat1,float lon1,float lat2,float lon2){
+trackerServoData setDirection(trackerServoData servoData, GPSLocation curLoc, GPSLocation newLoc){
   //Determine heading
-  float direction = heading(lat1,lon1,lat2,lon2);
-  int serialPosition = 1000;
+  float direction = heading(curLoc, newLoc);
 
   //Figure out if it is to the front or to the back
   if (abs(direction) < 90){
     Serial.println("Heading is in front of the device");
-    objectFront = true;
-    serialPosition = map(direction, -90, 90, servoMin, servoMax);
+    servoData.objectFront = true;
+    servoData.directionLocation = map(direction, -90, 90, servoData.directionMin, servoData.directionMax);
+    return servoData;
   }
-  else{
-    Serial.println("Heading is behind the device");
-    objectFront = false;
-    if(direction > 0){
+
+  Serial.println("Heading is behind the device");
+  servoData.objectFront = false;
+
+  if(direction > 0){
       Serial.println("Heading is to the right");
-      serialPosition = map(direction, 91, 180, servoCenter, servoMin);
-    }
-    else{
-      Serial.println("Heading is to the left");
-      serialPosition = map(direction, -91, -180, servoCenter, servoMax);
-    }
+      servoData.directionLocation = map(direction, 91, 180, servoData.directionCenter, servoData.directionMin);
+      return servoData;
   }
-  Serial.print("Servo set to number:");
-  Serial.println(serialPosition);
-  directionservo.write(serialPosition);
-  delay(1000);
+
+  Serial.println("Heading is to the left");
+  servoData.directionLocation = map(direction, -91, -180, servoData.directionCenter, servoData.directionMax);
+  return servoData;
 }
 void loop() {
   // put your main code here, to run repeatedly:
